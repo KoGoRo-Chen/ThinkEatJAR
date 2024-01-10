9a630d0ffbb48a52db303f1bcb3f04c32a38b6a1
KoGoRo-Chen <kogoro501@gmail.com>
2024-01-10 15:04:16 +0800

    Start Transfering Entity to Spring Data JPA

--- ThinkEat/src/test/java/ThinkEat/mvc/ThinkEatApplicationTests.java
+++ ThinkEat/src/test/java/ThinkEat/mvc/ThinkEatApplicationTests.java
@@ -4,59 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import ThinkEat.mvc.dao.demo.ArticleDao;
-import ThinkEat.mvc.dao.demo.EmployeeDao;
-import ThinkEat.mvc.OldBean.demo.Article;
-import ThinkEat.mvc.OldBean.demo.Employee;
 
 @SpringBootTest
 class ThinkEatApplicationTests {
-
-	@Autowired
-	EmployeeDao employeeDao;
-	
-	@Autowired
-	ArticleDao articleDao;
-	
-	@Test
-	void test1() {
-		
-		Employee employee = new Employee();
-		employee.setUsername("aweit-zhu");
-		employee.setPassword("123");
-		employee.setUserNickName("aweit");
-		
-		employeeDao.save(employee);
-		
-		System.out.println(employee.getUserId());
-
-	}
-	
-	@Test
-	void test2() {
-		
-		Employee e1 = employeeDao.findById(1).get();
-		
-		Article a1 = new Article();
-		a1.setTitle("title1");
-		a1.setEmployee(e1);
-		articleDao.save(a1);
-		
-		Article a2 = new Article();
-		a2.setTitle("title2");
-		a2.setEmployee(e1);
-		articleDao.save(a2);
-
-	}
-	
-	//@Test
-	//@Transactional
-	void test3() {
-		
-		Employee employee = employeeDao.findById(1).get();
-
-		System.out.println(employee);
-
-	}
 
 }
