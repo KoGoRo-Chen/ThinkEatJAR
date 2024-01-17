f6171a839d4923c0c932107000b839adee807cec
KoGoRo-Chen <kogoro501@gmail.com>
2024-01-17 20:45:31 +0800

    完成多張圖片上傳+顯示功能

--- ThinkEat/src/main/java/ThinkEat/mvc/controller/TestController.java
+++ ThinkEat/src/main/java/ThinkEat/mvc/controller/TestController.java
@@ -12,6 +12,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("Test/")
@@ -36,54 +38,18 @@ public class TestController {
 
     //處理圖片上傳
     @PostMapping("/Upload")
-    public String UploadPicture(@RequestPart("picture") MultipartFile multipartFile,
-                                HttpServletResponse response,
-                                Model model) {
-        PictureDto pictureDto = new PictureDto();
-        Integer picId = pictureService.addPicture(pictureDto, multipartFile);
-        model.addAttribute("pictureDto", pictureDto);
-        System.out.println(pictureService.getPictureById(picId).getPath());
-        model.addAttribute("imagePath", pictureService.getPictureById(picId).getPath());
-        return "Test/PicUploadTest";
-    }
-
-    @GetMapping("/image")
-    public String getImage(Model model) {
-        String imagePath = servletPath + "1705405519228_OIP.jpg"; // 靜態資源的相對路徑
-        model.addAttribute("imagePath", imagePath);
-        // Thymeleaf的頁面名稱
+    public String uploadPictures(@RequestPart("pictures") List<MultipartFile> multipartFileList,
+                                 Model model) {
+        List<String> imagePaths = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFileList) {
+            PictureDto pictureDto = new PictureDto();
+            Integer picId = pictureService.addPicture(pictureDto, multipartFile);
+            imagePaths.add(pictureService.getPictureById(picId).getPath());
+        }
+        model.addAttribute("imagePaths", imagePaths);
         return "Test/PicUploadTest";
     }
 
 
-//    //顯示圖片
-//    @GetMapping("/Get")
-//    @ResponseBody
-//    public ResponseEntity<byte[]> getImage(@RequestParam String fileName) throws IOException {
-//        Path imagePath = Paths.get(IMAGE_FOLDER, fileName);
-//
-//        // 讀取圖片為byte陣列
-//        byte[] imageBytes = Files.readAllBytes(imagePath);
-//
-//        // 將byte陣列轉換為BufferedImage
-//        BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(imageBytes));
-//
-//        // 處理BufferedImage（這裡可以添加您的圖片處理邏輯）
-//
-//        // 將處理後的BufferedImage轉換為byte陣列
-//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//        ImageIO.write(bufferedImage, "jpg", byteArrayOutputStream);
-//        byte[] processedImageBytes = byteArrayOutputStream.toByteArray();
-//
-//        // 設定HTTP頭部，告訴瀏覽器圖片的MIME類型
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.IMAGE_JPEG);
-//
-//        // 返回ResponseEntity，包含圖片資料和HTTP頭部
-//        return ResponseEntity.ok()
-//                .headers(headers)
-//                .contentLength(processedImageBytes.length)
-//                .body(processedImageBytes);
-//    }
 }
 
