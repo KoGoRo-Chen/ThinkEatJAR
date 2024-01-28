71eaf93fdcd0ba4c87806be09ba6194e44e2565c
KoGoRo-Chen <kogoro501@gmail.com>
2024-01-28 22:49:55 +0800

    部署到tomcat上出現問題

--- ThinkEat/src/main/java/ThinkEat/mvc/ThinkEatApplication.java
+++ ThinkEat/src/main/java/ThinkEat/mvc/ThinkEatApplication.java
@@ -2,14 +2,24 @@ package ThinkEat.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ServletComponentScan
-public class ThinkEatApplication {
+public class ThinkEatApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ThinkEatApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ThinkEatApplication.class);
+    }
 }
+
